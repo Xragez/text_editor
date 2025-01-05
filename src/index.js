bb4a9fab25dbab265ea1ae2507d12392275fc419
Paweł Zegarski <pawelzeg@gmail.com>
2025-01-05 11:19:40 +0100

    Caret movement

--- src/index.js
+++ src/index.js
@@ -11,34 +11,15 @@ class Caret {
         this.#updateCaretPosition();
     }
 
-    #updateCaretPosition() {
-        console.log(this.offset);
-        if (!this.line) return;
-        
-        const range = document.createRange();
-        const selection = window.getSelection();
-    
-        range.setStart(this.line.firstChild || this.line, Math.min(this.offset, this.line.textContent.length));
-        range.collapse(true);
-    
-        selection.removeAllRanges();
-        selection.addRange(range);
-    
-        this.line.scrollIntoView({ behavior: "smooth", block: "nearest" });
-    }
-
     moveLeft() {
         const lineLen = this.line.textContent.length;
-        if (lineLen < this.offset && lineLen != 0) {
+
+        if (lineLen > 0 && this.offset > lineLen) {
             this.offset = lineLen - 1;
         } else if (lineLen > 0 && this.offset > 0) {
             this.offset--;
         } else {
-            const prevLine = this.line.previousElementSibling;
-            if (prevLine) {
-                this.line = prevLine;
-                this.offset = prevLine.textContent.length;
-          }
+            this.#goToEndOfPrevLine();
         }
         this.#updateCaretPosition();
     }
@@ -47,11 +28,7 @@ class Caret {
         if (this.offset < this.line.textContent.length) {
           this.offset++;
         } else {
-          const nextLine = this.line.nextElementSibling;
-          if (nextLine) {
-            this.line = nextLine;
-            this.offset = 0;
-          }
+            this.#goToStartOfNextLine();
         }
         this.#updateCaretPosition();
     }
@@ -71,30 +48,78 @@ class Caret {
         }
         this.#updateCaretPosition();
     }
+
+    calculateOffset(line) {
+        const selection = window.getSelection();
+        if (!selection.rangeCount) return 0;
+        
+        const range = selection.getRangeAt(0);
+        
+        if (range.startContainer === line.firstChild || range.startContainer === line) {
+          return range.startOffset;
+        }
+        
+        return 0;
+    }
+
+    #updateCaretPosition() {
+        if (!this.line) return;
+        
+        const range = document.createRange();
+        const selection = window.getSelection();
+    
+        range.setStart(this.line.firstChild || this.line, Math.min(this.offset, this.line.textContent.length));
+        range.collapse(true);
+    
+        selection.removeAllRanges();
+        selection.addRange(range);
+    
+        this.line.scrollIntoView({ behavior: "smooth", block: "nearest" });
+    }  
+
+    #goToEndOfPrevLine() {
+        const prevLine = this.line.previousElementSibling;
+        if (prevLine) {
+            this.line = prevLine;
+            this.offset = prevLine.textContent.length;
+        }
+    }
+
+    #goToStartOfNextLine() {
+        const nextLine = this.line.nextElementSibling;
+        if (nextLine) {
+            this.line = nextLine;
+            this.offset = 0;
+        }
+    }
 }
 
 class EditorWindow {
     constructor(editor) {
-        this.editor = editor;
+        this._editor = editor;
+
         const newLine = this.#addNewLine(null, 0);
-
         this.caret = new Caret(newLine);
+
+        // caret movement
+        this.#arrowsMovementHandler();
+        this.#leftMouseClickHandler();
+
+        // text editing
         
-        this.#arrowsMovementHandler()
-
-        this._fileService = new FileService();
-
-        const fileInput = document.getElementById("fileInput");
-        fileInput.addEventListener("change", async (event) => {
-            const file = event.target.files[0];
-            const content = await this._fileService.readFromFile(file);
-            console.log(content);
-            this.#loadTextToEditor(content);
-        })
     }
 
-    getCaret() {
-        return this.caret;
+    set dataSource(source) {
+        this._dataSource = source;
+    }
+
+    async loadData() {
+        if (this._dataSource) {
+            const content = await this._dataSource.readData();
+            this.#loadTextToEditor(content);
+        } else {
+            new Error("No file selected");
+        }
     }
 
     #addNewLine(text, index) {
@@ -104,47 +129,60 @@ class EditorWindow {
         lineDiv.textContent = text || ''; 
         lineDiv.contentEditable = true;
 
-        this.editor.appendChild(lineDiv);
+        this._editor.appendChild(lineDiv);
 
         return lineDiv;
     }
 
     #loadTextToEditor(content) {
-        this.editor.innerHTML = '';
+        this._editor.innerHTML = '';
         const lines = content.split("\n");
 
         lines.forEach((line, index) => {
             this.#addNewLine(line, index);
         });
 
-        this.caret.setPosition(this.editor.lastChild, 0)
+        this.caret.setPosition(this._editor.lastChild, 0)
     }
 
     #arrowsMovementHandler() {
-        this.editor.addEventListener("keydown", (event) => {
-            const activeElement = document.activeElement;
-    
-            if (activeElement && activeElement.classList.contains("editor-line")) {
-                if (event.key === "ArrowUp") {
-                    this.caret.moveUp();
-                    event.preventDefault();
-                } else if (event.key === "ArrowDown") {
-                    this.caret.moveDown();
-                    event.preventDefault();
-                } else if (event.key === "ArrowLeft") {
-                    this.caret.moveLeft();
-                    event.preventDefault();
-                } else if (event.key === "ArrowRight") {
-                    this.caret.moveRight();
-                    event.preventDefault();
-                }
+        this._editor.addEventListener("keydown", (event) => {
+            if (event.key === "ArrowUp") {
+                this.caret.moveUp();
+                event.preventDefault();
+            } else if (event.key === "ArrowDown") {
+                this.caret.moveDown();
+                event.preventDefault();
+            } else if (event.key === "ArrowLeft") {
+                this.caret.moveLeft();
+                event.preventDefault();
+            } else if (event.key === "ArrowRight") {
+                this.caret.moveRight();
+                event.preventDefault();
             }
         });
     }
+
+    #leftMouseClickHandler() {
+        this._editor.addEventListener("click", (event) => {
+            if (event.target.id === this._editor.id) {
+                const lastLine = this._editor.lastChild;
+                this.caret.setPosition(lastLine, lastLine.textContent.length);
+            } else {
+                const line = event.target;
+                const offset = this.caret.calculateOffset(line);
+                this.caret.setPosition(line, offset);
+            }
+        })
+    }
 }
 
-class FileService {
-    async readFromFile(file) {
+class FileSource {
+    constructor(file) {
+        this.file = file
+    }
+
+    async readData() {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
             reader.onload = (e) => {
@@ -152,8 +190,8 @@ class FileService {
                 resolve(fileContent);
             }
     
-            if (file) {
-                reader.readAsText(file);
+            if (this.file) {
+                reader.readAsText(this.file);
             } else {
                 reject(new Error("No file selected"))
             }
@@ -163,3 +201,12 @@ class FileService {
 
 const editor = document.getElementById("editor");
 const editorWindow = new EditorWindow(editor);
+
+const fileInput = document.getElementById("fileInput");
+fileInput.addEventListener("change", async (event) => {
+    const file = event.target.files[0];
+    const fileSource = new FileSource(file);
+
+    editorWindow.dataSource = fileSource;
+    await editorWindow.loadData();
+});
